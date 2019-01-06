ca0a66c126c01b35cda6c2a41ddbde507f287cbe
yunsheng <helloyunsheng@126.com>
2019-01-06 22:39:30 +0800

    Initial commit

---
+++ eureka_client/src/main/java/com/yunsheng/eureka_client/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.yunsheng.eureka_client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "hi " + name;
+    }
+}
