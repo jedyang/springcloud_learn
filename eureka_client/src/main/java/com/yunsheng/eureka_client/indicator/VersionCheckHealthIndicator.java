2523517c2b6be0e7abb7a9f6b37bddab3025cf2b
杨云胜 <yangyunsheng@haier.com>
2019-01-16 13:54:02 +0800

    监控

--- eureka_client/src/main/java/com/yunsheng/eureka_client/indicator/VersionCheckHealthIndicator.java
+++ eureka_client/src/main/java/com/yunsheng/eureka_client/indicator/VersionCheckHealthIndicator.java
@@ -3,11 +3,13 @@ package com.yunsheng.eureka_client.indicator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class MyHealthIndicator extends AbstractHealthIndicator {
+@Component
+public class VersionCheckHealthIndicator extends AbstractHealthIndicator {
 
     @Value("${version}")
     String version;
@@ -17,7 +19,7 @@ public class MyHealthIndicator extends AbstractHealthIndicator {
         if ("2.0.0".equalsIgnoreCase(version)) {
             builder.up();
         } else {
-            log.warn("版本过低");
+            log.warn("版本过低111");
             builder.down();
         }
         builder.withDetail("version", version);
