6c7489954f25553984cec0e8c686242c78c20f26
yunsheng <helloyunsheng@126.com>
2019-01-26 17:27:35 +0800

    手动刷新

--- eureka_consumer/ribbon/src/main/java/com/yunsheng/ribbon/controller/ConsumeHelloController.java
+++ eureka_consumer/ribbon/src/main/java/com/yunsheng/ribbon/controller/ConsumeHelloController.java
@@ -4,11 +4,13 @@ import com.yunsheng.ribbon.service.HelloService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConsumeHelloController {
     @Autowired
     HelloService helloService;
